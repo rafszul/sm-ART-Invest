0929963b994c7c2a62b5eca4fe9ed23604e549b1
rafszul <rafszul@gmail.com>
2017-02-21 19:43:49 +0000

    works

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'smart works!';
+  title = 'ART';
+
 }
+
+// https://s-media-cache-ak0.pinimg.com/originals/04/c7/27/04c7278f4ae9e4738cf4a2d9d99bf343.jpg
+// https://s-media-cache-ak0.pinimg.com/originals/04/8c/31/048c31951a7aa84f4d8dff1d9097bd62.jpg
+// https://s-media-cache-ak0.pinimg.com/originals/ac/8c/4c/ac8c4c248876f88b39c0281c05258338.jpg
+// https://s-media-cache-ak0.pinimg.com/originals/06/c5/f1/06c5f1ac24384bd360e02b5d2b24d9f7.jpg
+// https://s-media-cache-ak0.pinimg.com/originals/54/e4/54/54e4545520448e766236d696853c981d.jpg
+// https://s-media-cache-ak0.pinimg.com/originals/a1/5d/33/a15d33d0e5e23e5d2c76efec0c2eb909.jpg
+// https://s-media-cache-ak0.pinimg.com/originals/e2/43/da/e243da42b40387f15c75449a3d99d508.jpg
+// https://s-media-cache-ak0.pinimg.com/originals/48/6c/16/486c16b3da7158dec3ce11a0fba242e9.jpg
+// https://s-media-cache-ak0.pinimg.com/originals/6a/41/15/6a41153a9ecfe13ffe72e3ffb2b0cd61.jpg
+// https://s-media-cache-ak0.pinimg.com/originals/c1/71/45/c17145ccb797d425e168dabeb0e3e0d1.jpg
